4d55db26a012d4cd4850c3e5414ec1b91d8f9d7b
Heidi Meiners <0266159@up.edu.mx>
2025-05-22 11:03:40 -0600

    Bling-Phong

--- src/TheRayTracer/Raytracer.java
+++ src/TheRayTracer/Raytracer.java
@@ -21,22 +21,22 @@ public class Raytracer {
         Scene scene01 = new Scene();
         TransformObject transform = new TransformObject();
 
-        scene01.setCamera(new Camera(new Vector3D(0, 0, -4), 60, 55,
-                1920, 1080, 0.6, 50.0));
+        scene01.setCamera(new Camera(new Vector3D(0, 0, -4), 60, 60,
+                4096, 2160, 0.6, 50.0));
 
-        //scene01.addLight(new PointLight(new Vector3D(1, 0, 0), Color.WHITE, 3));
-        scene01.addLight(new PointLight(new Vector3D(0, 5, -3), Color.WHITE, 3));
-        scene01.addLight(new PointLight(new Vector3D(0, 0, -3), Color.WHITE, 3));
+        scene01.addLight(new PointLight(new Vector3D(0, 5, 2), Color.WHITE, 3));
+        scene01.addLight(new PointLight(new Vector3D(2, 1, -3), Color.WHITE, 3));
+        scene01.addLight(new PointLight(new Vector3D(-2, 1, -3), Color.WHITE, 3));
         //scene01.addLight(new DirectionalLight(new Vector3D(0,0,1), Color.white,.5));
 
         scene01.addObject(new Model3D(new Vector3D(0, -2, 0), new Triangle[]{
                 new Triangle(new Vector3D(-5, 0, -5), new Vector3D(5, 0, -5), new Vector3D(5, 0, 5)),
                 new Triangle(new Vector3D(-5, 0, -5), new Vector3D(5, 0, 5), new Vector3D(-5, 0, 5))},
                 Color.LIGHT_GRAY));
-        scene01.addObject(new Sphere(new Vector3D(0, 0, -1), 1, Color.BLUE));
-        //scene01.addObject(OBJReader.getModel3D("C:\\ISGC\\4_Semestre_ISGC\\Graficas_computacionales\\3Parcial\\Objects\\SmallTeapot.obj", new Vector3D(0, -2, 2), Color.red, 2, 0));
+        //scene01.addObject(new Sphere(new Vector3D(0, 0, -1), 1, Color.BLUE));
+        scene01.addObject(OBJReader.getModel3D("C:\\ISGC\\4_Semestre_ISGC\\Graficas_computacionales\\3Parcial\\Objects\\SmallTeapot.obj", new Vector3D(0, -2, 2), Color.red, 1.5, 0));
         //scene01.addObject(OBJReader.getModel3D("C:\\ISGC\\4_Semestre_ISGC\\Graficas_computacionales\\3Parcial\\FirstScene\\Coffe_table.obj",new Vector3D(0, -3, 2),new Color(88,57,39)));
-        //scene01.addObject(OBJReader.getModel3D("C:\\ISGC\\4_Semestre_ISGC\\Graficas_computacionales\\3Parcial\\FirstScene\\glass.obj",new Vector3D(3,-3,2),Color.blue));
+        //scene01.addObject(OBJReader.getModel3D("C:\\ISGC\\4_Semestre_ISGC\\Graficas_computacionales\\3Parcial\\FirstScene\\glass.obj",new Vector3D(0,-2,2),Color.blue,1,0));
         //scene01.addObject(OBJReader.getModel3D("C:\\ISGC\\4_Semestre_ISGC\\Graficas_computacionales\\3Parcial\\FirstScene\\CanOBJ.obj",new Vector3D(-3,-3,2),Color.gray));
         //scene01.addObject(OBJReader.getModel3D("C:\\ISGC\\4_Semestre_ISGC\\Graficas_computacionales\\3Parcial\\FirstScene\\cupa.obj.obj",new Vector3D(3,3,2),Color.white));
         //scene01.addObject(OBJReader.getModel3D("C:\\ISGC\\4_Semestre_ISGC\\Graficas_computacionales\\3Parcial\\FirstScene\\bolt.obj",new Vector3D(-3,3,-2),Color.gray));
@@ -74,7 +74,12 @@ public class Raytracer {
                 Color pixelColor = Color.black;
                 if (closestIntersection != null) {
                     Color objColor = closestIntersection.getObject().getColor();
-
+                    //ambient light
+                    int redAmbient= (int)(objColor.getRed()*.1);
+                    int greenAmbient= (int)(objColor.getGreen()*.1);
+                    int blueAmbient= (int)(objColor.getBlue()*.1);
+                    Color ambient=new Color(redAmbient,greenAmbient,blueAmbient);
+                    pixelColor = addColor(pixelColor, ambient);
                     for (Light light : lights) {
                         if (light.isInShadow(closestIntersection.getPosition(), closestIntersection.getNormal(), objects)) {
                             continue;
@@ -101,7 +106,21 @@ public class Raytracer {
                                 (float) Math.clamp(objColors[1], 0.0, 1.0),
                                 (float) Math.clamp(objColors[2], 0.0, 1.0));
 
+                        Vector3D viewDir = Vector3D.normalize(Vector3D.substract(scene.getCamera().getPosition(), closestIntersection.getPosition()));
+                        Vector3D lightDir = Vector3D.normalize(Vector3D.substract(light.getPosition(), closestIntersection.getPosition()));
+                        Vector3D reflectDir = Vector3D.substract(Vector3D.scalarMultiplication(lightDir,-1), Vector3D.scalarMultiplication(closestIntersection.getNormal(),2 * Vector3D.dotProduct(closestIntersection.getNormal(), Vector3D.scalarMultiplication(lightDir,-1))));
+                        double spec = Math.pow(Math.max(0.0, Vector3D.dotProduct(viewDir, reflectDir)), 100); // shininess = 32
+                        double ks = 1; // coeficiente especular
+
+                        Color specular = new Color(
+                                (float)Math.clamp(ks * lightColors[0] * spec, 0.0, 1.0),
+                                (float)Math.clamp(ks * lightColors[1] * spec, 0.0, 1.0),
+                                (float)Math.clamp(ks * lightColors[2] * spec, 0.0, 1.0)
+                        );
+
+
                         pixelColor = addColor(pixelColor, diffuse);
+                        pixelColor = addColor(pixelColor, specular);
                     }
                 }
                 image.setRGB(i, j, pixelColor.getRGB());
